2aeeee94b4d72e8a44cf77e42fcd7dfc7cb1411b
shawn <shawnsandy04@gmail.com>
2023-03-24 14:32:57 -0400

    test: 💣 add vitest-axe and remove snapshot test

--- apps/fpkit-nextjs/src/components/nav/nav.test.tsx
+++ apps/fpkit-nextjs/src/components/nav/nav.test.tsx
@@ -2,11 +2,15 @@ import React from "react";
 
 import { describe, expect, test, it } from "vitest";
 import { render, screen } from "@testing-library/react";
+import { axe } from "vitest-axe";
 
 import Nav from "./nav";
 describe("App renders correctly", () => {
-  test("renders without crashing", () => {
+  test("renders without crashing", async () => {
     render(<Nav />);
-    expect(screen).toMatchSnapshot();
+    const nav = screen.getByRole("navigation");
+    expect(nav).toBeDefined();
+    expect(await axe(nav)).toHaveNoViolations();
+    // expect(nav).toMatchSnapshot();
   });
 });
--- apps/fpkit-nextjs/src/test/setup.ts
+++ apps/fpkit-nextjs/src/test/setup.ts
@@ -1,6 +1,7 @@
 import { expect, afterEach } from "vitest";
 import { cleanup } from "@testing-library/react";
-import matchers from "@testing-library/jest-dom/matchers";
+import * as matchers from "vitest-axe/matchers";
+import "vitest-axe/extend-expect";
 
 // extends Vitest's expect method with methods from react-testing-library
 expect.extend(matchers);

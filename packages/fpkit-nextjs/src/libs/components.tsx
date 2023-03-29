2523af9fcd0a69e700bd67184ee3aa10a8181500
shawn <shawnsandy04@gmail.com>
2023-03-29 09:02:20 -0400

    feat: 🚗 add post list page

--- packages/fpkit-nextjs/src/libs/components.tsx
+++ packages/fpkit-nextjs/src/libs/components.tsx
@@ -5,5 +5,5 @@ type ComponentTypes = {
 
 export const MDXComponents = {
   h1: ({ children }: ComponentTypes) => <h1 data-style>{children}</h1>,
-  p: ({ children }: ComponentTypes) => <p>{children}</p>,
+  p: ({ children }: ComponentTypes) => <p data-style>{children}</p>,
 };

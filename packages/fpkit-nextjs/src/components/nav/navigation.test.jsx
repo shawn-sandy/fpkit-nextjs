a2e362052a2bc7ba287a9e83e24ecddb8ccbe2ef
shawn <shawnsandy04@gmail.com>
2023-03-31 19:08:04 -0400

    test: 💣 add snapshot test for navigation component

--- packages/fpkit-nextjs/src/components/nav/navigation.test.jsx
+++ packages/fpkit-nextjs/src/components/nav/navigation.test.jsx
@@ -4,7 +4,7 @@ import { render, screen } from "@testing-library/react";
 import { axe } from "vitest-axe";
 import { expect, describe, test } from "vitest";
 
-import { dataSet, branding } from "../mocks/navigation.mocks";
+import { dataSet, branding } from "../data/navigation.mocks";
 
 // 👉 import your component here
 import Navigation from "./navigation";
@@ -13,7 +13,6 @@ describe("Navigation", () => {
   test("renders the navigation", () => {
     render(<Navigation data={dataSet} />);
     const navigation = screen.getByRole("navigation");
-    // const brand = screen.getByRole("heading");
 
     expect(navigation).toBeDefined();
     expect(navigation).toMatchSnapshot();
@@ -25,7 +24,7 @@ describe("Navigation", () => {
     expect(await axe(container)).toHaveNoViolations();
   });
 
-  test("renders the branding", async () => {
+  test("renders the branding w/navigation", async () => {
     render(<Navigation data={dataSet} brand={branding} />);
     const navigation = screen.getByRole("navigation");
     const brand = screen.getByRole("heading");
@@ -33,5 +32,6 @@ describe("Navigation", () => {
     expect(brand).toBeDefined();
     expect(brand).toMatchSnapshot();
     expect(await axe(navigation)).toHaveNoViolations();
+    expect(navigation).toMatchSnapshot();
   });
 });

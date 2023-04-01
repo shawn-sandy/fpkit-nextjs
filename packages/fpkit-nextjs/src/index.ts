90b2bf2b527da0d758d173f5e01c68fe2d134184
shawn <shawnsandy04@gmail.com>
2023-03-31 23:44:11 -0400

    build: 📦 combine libs, components, hooks into one package

--- packages/fpkit-nextjs/src/index.ts
+++ packages/fpkit-nextjs/src/index.ts
@@ -5,4 +5,10 @@ export * from './components/layout/main';
 export * from './components/layout/header';
 export * from './components/layout/footer';
 export * from './components/layout/landmarks';
-export * from './components/posts/post-list'
+export * from './components/posts/post-list';
+
+export { usePageOpts } from './hooks/useOpts';
+export { useConfig } from './hooks/useConfig';
+
+export * from './libs/components'
+export * from './libs/filter-mdx-pages'

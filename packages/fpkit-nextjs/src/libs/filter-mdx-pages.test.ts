6c673ecc70ed807aa78b0c9e5d4eab5181d02163
shawn <shawnsandy04@gmail.com>
2023-04-09 20:14:40 -0400

    test: 🧪 move sample date into /tests/test-data.ts

--- packages/fpkit-nextjs/src/libs/filter-mdx-pages.test.ts
+++ packages/fpkit-nextjs/src/libs/filter-mdx-pages.test.ts
@@ -1,89 +1,13 @@
-import { FilterMdxPages } from './filter-mdx-pages';
+import { FilterMdxPages, FilterPageType } from './filter-mdx-pages';
 import { describe, it, assert } from "vitest";
-import FilterPageType from "./filter-mdx-pages";
 import { MdxFile, PageMapItem } from "nextra";
 
-describe("test _fpkit_nextjs", function () {
-  it("test @fpkit/nextjs.FilterPageType", function (done) {
-    const pages: MdxFile[] = [
-      {
-        // @ts-ignore
-        kind: "Folder",
-        name: "pages",
-        path: "pages/",
-        children: [
-          {
-            kind: "MdxPage",
-            name: "index",
-            path: "pages/index.mdx",
-            frontMatter: {
-              title: "Home",
-              type: "page",
-            },
-          },
-          {
-            kind: "MdxPage",
-            name: "about",
-            path: "pages/about.mdx",
-            frontMatter: {
-              title: "About",
-              type: "page",
-            },
-          },
-          {
-            kind: "Folder",
-            name: "blog",
-            path: "pages/blog/",
-            children: [
-              {
-                kind: "MdxPage",
-                name: "index",
-                path: "pages/blog/index.mdx",
-                frontMatter: {
-                  title: "Blog",
-                  type: "page",
-                },
-              },
-              {
-                kind: "MdxPage",
-                name: "first-post",
-                path: "pages/blog/first-post.mdx",
-                frontMatter: {
-                  title: "First Post",
-                  type: "page",
-                },
-              },
-              {
-                kind: "MdxPage",
-                name: "second-post",
-                path: "pages/blog/second-post.mdx",
-                frontMatter: {
-                  title: "Second Post",
-                  type: "page",
-                },
-              },
-            ],
-          },
-          {
-            kind: "MdxPage",
-            name: "contact",
-            path: "pages/contact.mdx",
-            frontMatter: {
-              title: "Contact",
-              type: "page",
-            },
-          },
-        ],
-      },
-    ];
-    const results = FilterPageType(pages);
-    console.log(results);
-  });
-});
+import { pageMap } from './tests/test-data';
+
 
 
 describe('test _fpkit_nextjs', function () {
-  it('test it returns and empty results', function (done) {
+  it('test it returns empty results', function (done) {
     let pages = [{
       "kind": "MdxPage",
       "frontMatter": {
@@ -99,28 +23,24 @@ describe('test _fpkit_nextjs', function () {
     let result = FilterMdxPages(pages, sortBy, sortOrder);
     assert.deepEqual(result, []);
     console.log(result);
-
     // done();
   }),
 
-    it('it returns results', function (done) {
-      let pages = [{
-        "kind": "MdxPage",
-        "frontMatter": {
-          "title": "Test Page",
-          "description": "Test Description",
-          "type": "page"
-        },
-        "filePath": "pages\\test\\test.mdx"
-      }];
+    it('it returns the correct results', function () {
+
       let sortBy = 'date';
       let sortOrder = "descending";
       // @ts-ignore
-      let result = FilterMdxPages(pages, sortBy, sortOrder);
-      assert.deepEqual(result, []);
-      console.log(result);
-
-      // done();
+      let result = FilterMdxPages(pageMap.pageMap, sortBy, sortOrder);
+      expect(result).toHaveLength(3);
     }
     )
+
+
+  it("it get the correct page types", function (done) {
+    // @ts-ignore
+    const pages: PageMapItem[] = pageMap.pageMap;
+    const results = FilterPageType(pages);
+    expect(results).toHaveLength(3);
+  });
 })

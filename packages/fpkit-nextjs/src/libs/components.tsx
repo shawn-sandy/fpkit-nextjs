201032069a67c3184b37a15c0c7c1e5a98877957
shawn <shawnsandy04@gmail.com>
2023-04-09 20:57:28 -0400

    feat: add table w/div [data-scroll] wrapper to mdx components

--- packages/fpkit-nextjs/src/libs/components.tsx
+++ packages/fpkit-nextjs/src/libs/components.tsx
@@ -6,4 +6,7 @@ type ComponentTypes = {
 export const MDXComponents = {
   h1: ({ children }: ComponentTypes) => <h1 data-style>{children}</h1>,
   p: ({ children }: ComponentTypes) => <p data-style>{children}</p>,
+  table: ({ children }: ComponentTypes) => (
+    <table data-scroll>{children}</table>
+  ),
 };

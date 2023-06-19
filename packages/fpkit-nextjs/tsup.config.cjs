d1664ab89a389733f09fb458b6b91599950431eb
shawn <shawnsandy04@gmail.com>
2023-06-19 16:59:32 -0400

    feat(fpkit-nextjs): add hooks folder with usePageOpts and useConfig hooks
    fix(fpkit-nextjs): update tsup.config.cjs to include new hooks folder in entry array

--- packages/fpkit-nextjs/tsup.config.cjs
+++ packages/fpkit-nextjs/tsup.config.cjs
@@ -1,7 +1,7 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: ["src/index.ts", "src/hooks.ts", "src/libs.ts"],
+  entry: ["src/index.ts", "src/hooks.ts", "src/hooks/index.ts", "src/libs.ts"],
   outDir: "libs",
   splitting: true,
   sourcemap: true,

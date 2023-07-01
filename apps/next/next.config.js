61308d5445eb186eced024553a8ed4636079ab1d
shawn <shawnsandy04@gmail.com>
2023-07-01 16:07:11 -0400

    chore(next.config.js): update nextra and nextra-theme-docs versions to 2.8.0
    chore(package.json): update nextra and nextra-theme-docs versions to 2.8.0
    chore(theme.config.jsx): update logo and project link in theme configuration
    chore(theme.config.tsx): remove unused theme configuration file

--- apps/next/next.config.js
+++ apps/next/next.config.js
@@ -5,3 +5,9 @@ const nextConfig = {};
 
 // If you have other Next.js configurations, you can pass them as the parameter:
 // module.exports = withNextra({ /* other next.js config */ })
+const withNextra = require("nextra")({
+  theme: "nextra-theme-docs",
+  themeConfig: "./theme.config.jsx",
+});
+
+module.exports = withNextra();

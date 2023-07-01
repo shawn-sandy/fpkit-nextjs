007831c4f6a339d2624a99a4c49516ef036307d1
shawn <shawnsandy04@gmail.com>
2023-07-01 16:24:27 -0400

    feat(index): 🚧 add navigation bar with links to Home, Blog, and About pages
    chore(themes): add import statement for "@shawnsandy/first-paint" CSS library

--- apps/next/src/themes/index.tsx
+++ apps/next/src/themes/index.tsx
@@ -1,11 +1,25 @@
 import Link from "next/link";
 import type { NextraThemeLayoutProps } from "nextra";
-
+import "@shawnsandy/first-paint/dist/css/libs/all.min.css";
+import { PG } from "@fpkit/nextjs";
 export default function Layout({ children, pageOpts }: NextraThemeLayoutProps) {
   const { pageMap } = pageOpts;
 
   return (
     <div>
+      <PG.Nav>
+        <div>
+          <a href="/">Home</a>
+        </div>
+        <ul>
+          <li>
+            <a href="/blog">Blog</a>
+          </li>
+          <li>
+            <a href="/about">About</a>
+          </li>
+        </ul>
+      </PG.Nav>
       <h1>My Theme</h1>
       {pageMap.map((item) => {
         if (item.kind === "MdxPage") {

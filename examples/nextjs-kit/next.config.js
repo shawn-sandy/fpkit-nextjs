387c60b1408b44142900d5ebcc68a912299cc062
shawn <shawnsandy04@gmail.com>
2023-07-02 12:34:41 -0400

    chore(next.config): uncomment withNextra configuration and remove commented code for better code readability and maintainability

--- examples/nextjs-kit/next.config.js
+++ examples/nextjs-kit/next.config.js
@@ -5,9 +5,9 @@ const nextConfig = {};
 
 // If you have other Next.js configurations, you can pass them as the parameter:
 // module.exports = withNextra({ /* other next.js config */ })
-// const withNextra = require("nextra")({
-//   theme: "./src/themes/index.tsx",
-//   themeConfig: "./theme.config.jsx",
-// });
+const withNextra = require("nextra")({
+  theme: "./src/themes/index.tsx",
+  themeConfig: "./theme.config.jsx",
+});
 
-// module.exports = withNextra();
+module.exports = withNextra();

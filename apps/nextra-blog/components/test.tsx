1aa5bae864ab7834cbf0ce23ab52dbb1f75d893c
shawn <shawnsandy04@gmail.com>
2023-07-05 08:10:03 -0400

    refactor(test.tsx): rename PageList component to PostsList for better clarity and consistency
    feat(test.tsx): add showDescription prop to PostsList component to display post descriptions

--- apps/nextra-blog/components/test.tsx
+++ apps/nextra-blog/components/test.tsx
@@ -1,9 +1,10 @@
 import { getPagesUnderRoute, getAllPages } from "nextra/context";
 import { PageList } from "@fpkit/nextjs";
+import { PostsList } from "@fpkit/nextjs";
 export const Test = ({ frontMatter }) => {
   const pages = getAllPages();
   console.log({ pages });
-  return <PageList data={getAllPages()} />;
+  return <PostsList postList={getAllPages()} showDescription />;
 };
 
 export default Test;
